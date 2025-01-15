776b676be1706adb45145427053ff4378f963006
tevelsho <tevelshorz@gmail.com>
2025-01-15 20:57:49 +0800

    feat: template files + dsa recommendations

---
+++ src/Actor.h
@@ -0,0 +1,15 @@
+/*
+----------------------------------------------------------------------------
+Class: Actor (Actor.h)
+Description: Represents a movie with its details and the actors involved.
+ 
+Team Information:
+    - Member 1: [Name], [Student ID], [Group Name]
+    - Member 2: [Name], [Student ID], [Group Name]
+
+Key Features:
+    - Stores details about the movie (title, plot, release year).
+    - Maintains a list of actors who starred in the movie.
+    - Provides methods to update movie details and retrieve actor information.
+----------------------------------------------------------------------------
+*/
---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+DATA STRUCTURES & ALGORITHMS ASSIGNMENT
+    CODERS: Tevel Sho (S10258591B) & Brayden Saga (S10258659C)
+    GROUP: 
+
+`main.cpp` contains ...
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+
+}
